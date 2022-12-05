bbe097fc5154ff9a4c5a34ea0944fd9496a1ad1b
ziplox <100974818+sercanfurunci@users.noreply.github.com>
2022-12-05 11:34:39 +0300

    todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos,setTodos] = useState([])
+    const [input,setInput] = useState("")
+
+
+    const addTodo = (event) => {
+        event.preventDefault();
+        setTodos([...todos, input]);
+        setInput('');
+    };
+
+    return (
+        <div>
+            <h1>Todo App</h1>
+            <form>
+                <input value={input} onChange={event => setInput(event.target.value)} />
+                <button type="submit" onClick={addTodo}>
+                    Add Todo
+                </button>
+            </form>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
